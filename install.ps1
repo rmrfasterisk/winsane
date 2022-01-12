481afea62a80c7a414016c123eb4bf7de7bd26e0
Sai Praneeth Reddy <spreddy617@gmail.com>
2022-01-13 00:39:12 +0530

    first commit before ...
    I inevitably screw my system

---
+++ install.ps1
@@ -0,0 +1,32 @@
+$pkgs = @(
+    "Microsoft.Edge.Dev"
+    "Microsoft.WindowsTerminal.Preview"
+    "WhatsApp.WhatsApp"
+    "Microsoft.VisualStudioCode"
+    "Google.AndroidStudio"
+    "Git.Git"
+    "GitHub.cli"
+    "GoLang.Go"
+    "HeroicGamesLauncher.HeroicGamesLauncher"
+    "Valve.Steam"
+    "Bitwarden.Bitwarden"
+    "Notion.Notion"
+    "voidtools.Everything"
+    "RARLab.WinRAR.Beta"
+    "Tonec.InternetDownloadManager"
+    # msstore
+    "XP8JK4HZBVF435" # Discord
+    "9NCBCSZSJRSB" # Spotify
+    "XP89DCGQ3K6VLD" # Power Toys
+    "9N97ZCKPD60Q" # Unigram
+    "XP8JK4HZBVF435" # Auto Dark Mode
+    "9N64SQZTB3LM" # MPV
+    "9PJZ3BTL5PV6" # IrfanView64
+    "9NBLGGH4Z1SP" # ShareX
+    "9P95ZZKTNRN4" # Powershell Preview
+    "9426MICRO-STARINTERNATION.MSICenter_kzh8wxbdkxb8p" # MSI Center
+)
+
+ForEach ($pkg in $pkgs) {
+    winget install --id $pkg --accept-source-agreements --accept-package-agreements --silent --force --exact
+}
